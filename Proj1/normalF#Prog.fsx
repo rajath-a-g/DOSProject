8caa33364f4f5d263d14567a028a6dff3e0c7200
Prajwala Nagaraj <prajwalanagraj@gmail.com>
2020-09-21 13:26:59 -0400

    Normal F# program wihtout actors for comparison

--- Proj1/normalF#Prog.fsx
+++ Proj1/normalF#Prog.fsx
@@ -6,27 +6,27 @@ open Akka.FSharp
 open System
 
 let perfectSquare n =
-    let h = n &&& 0xF
-    if (h > 9) then false
+    let h = n &&& (bigint 0xF)
+    if (h > 9I) then false
     else
-        if ( h <> 2 && h <> 3 && h <> 5 && h <> 6 && h <> 7 && h <> 8 ) then
-            let t = ((n |> double |> sqrt) + 0.5) |> floor|> int
+        if ( h <> 2I && h <> 3I && h <> 5I && h <> 6I && h <> 7I && h <> 8I ) then
+            let t = ((n |> double |> sqrt) + 0.5) |> floor|> bigint
             t*t = n
         else false
 
 let sumOfSquares x y = [x..y] |> List.map (fun x->x*x) |> List.sum
 
-let GotInput n k = for i in 1..n do
-                    let e = i+k-1
+let GotInput n k = for i in 1I..n do
+                    let e = i+k-1I
                     let val1 = sumOfSquares i e
-                    printfn " sumOfSquares: %i " val1 
+                    //printfn " sumOfSquares: %i " val1 
                     //check for perfect square
                     if perfectSquare val1 then
-                        printfn "Perfect Sqrs: %i " i
+                        printfn "Perfect Sqrs: %A " i
 
 
-let i1 = fsi.CommandLineArgs.[1] |> int
-let i2 = fsi.CommandLineArgs.[2] |> int
+let i1 = fsi.CommandLineArgs.[1] |> int |> bigint
+let i2 = fsi.CommandLineArgs.[2] |> int |> bigint
 GotInput  i1 i2
 
 System.Console.ReadLine() |> ignore
