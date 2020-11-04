829794c4e2cef9dd7ee35ea4df93b1df43f2eb44
Prajwala <56329359+PrajwalaNagaraj@users.noreply.github.com>
2020-11-04 07:27:02 -0500

    Update project3.fsx
    code refactoring with comments

--- project3Bonus/project3.fsx
+++ project3Bonus/project3.fsx
@@ -5,13 +5,9 @@ let main() =
     let numRequests = int fsi.CommandLineArgs.[2]
     let percentageDecimalFailingNodes = double fsi.CommandLineArgs.[3]
     //Reading number of nodes and requests to invoke pastry algorithm
-    // if numFailingNodes >= numNodes then
-    //     failwith "More number of failing nodes than number of nodes. Exiting...."
-    // else if numFailingNodes < 0 then
-    //     failwith "Not a valid number of nodes (It cannot be negative). Exiting...."
-    // else
+    //Also reading the percantage decimal of failing nodes to simulate the alogorithm with failures.
+    //If the numNodes is 100 and percentageDecimalFailingNodes is 0.1, it implies we fail (0.1*100 =>) 10 nodes in system.
     let numFailingNodes = percentageDecimalFailingNodes * (double numNodes) |> int
-    printfn "numNodes=%i numRequests=%i numFailingNodes=%i" numNodes numRequests numFailingNodes 
     PastrySimulator.pastry numNodes numRequests numFailingNodes
 
 main()
